b36303433536678c564c3692f5b85269d5a62585
hoyeon <fnfn0901@sookmyung.ac.kr>
2024-11-10 14:16:56 +0900

    feat: MatchingData Model 추가

--- walkmong/walkmong/MatchingApplyProcedure/Models/MatchingData.swift
+++ walkmong/walkmong/MatchingApplyProcedure/Models/MatchingData.swift
@@ -5,4 +5,19 @@
 //  Created by 신호연 on 11/10/24.
 //
 
-import Foundation
+import UIKit
+
+struct MatchingData {
+    let date: String
+    let matchingStatus: String
+    let puppyImageName: String
+    let name: String
+    let genderIconName: String
+    let size: String
+    let breed: String
+    let weight: String
+    let postContent: String
+    let location: String
+    let distance: String
+    let time: String
+}
