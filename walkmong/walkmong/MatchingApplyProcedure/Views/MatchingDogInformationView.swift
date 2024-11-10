031e685a6c996828c81c6a4a47502f4c9c0a36cf
hoyeon <fnfn0901@sookmyung.ac.kr>
2024-11-10 23:34:35 +0900

    feat: MatchingDogInformationView 레이아웃 및 스크롤뷰 설정 개선
    - UIScrollView(mainScrollView)와 contentView를 사용하여 스크롤 가능한 레이아웃 구현
    - imageScrollView와 페이지 컨트롤(pageControl)을 통한 이미지 스크롤 구현
    - profileFrame, infoFrame, walkInfoFrame, relatedInfoFrame, ownerInfoFrame 등의 UI 컴포넌트 배치
    - buttonFrame을 안전 영역(safe area) 하단에 고정
    - mainScrollView의 높이를 안전 영역(safe area) 기준으로 고정하고 buttonFrame과 연동
    - SnapKit 사용하여 Auto Layout 관리
    - 코드 가독성 및 구조 개선

--- walkmong/walkmong/MatchingApplyProcedure/Views/MatchingDogInformationView.swift
+++ walkmong/walkmong/MatchingApplyProcedure/Views/MatchingDogInformationView.swift
@@ -1,78 +1,155 @@
-//
-//  MatchingDogInformationView.swift
-//  walkmong
-//
-//  Created by 신호연 on 11/10/24.
-//
-
 import UIKit
 import SnapKit
 
 class MatchingDogInformationView: UIView, UIScrollViewDelegate {
     
     // MARK: - UI Components
+    private let mainScrollView = UIScrollView()
+    private let contentView = UIView()
     private let imageScrollView = UIScrollView()
-    private let imageContentView = UIView() // 콘텐츠를 담을 컨테이너 뷰
+    private let imageContentView = UIView()
     private let pageControl = UIPageControl()
     private var imageViews: [UIImageView] = []
+
+    private let profileFrame = UIView()
+    private let infoFrame = UIView()
+    private let walkInfoFrame = UIView()
+    private let relatedInfoFrame = UIView()
+    private let ownerInfoFrame = UIView()
+    private let buttonFrame = UIView()
     
     // MARK: - Initializer
     override init(frame: CGRect) {
         super.init(frame: frame)
         setupView()
+        setupFrames()
     }
     
     required init?(coder: NSCoder) {
         super.init(coder: coder)
         setupView()
+        setupFrames()
     }
     
     // MARK: - Setup Methods
     private func setupView() {
         backgroundColor = .white
         
-        // ScrollView 설정
-        addSubview(imageScrollView)
+        // Main ScrollView 설정
+        addSubview(mainScrollView)
+        mainScrollView.showsVerticalScrollIndicator = false
+        mainScrollView.snp.makeConstraints { make in
+            make.top.leading.trailing.equalToSuperview()
+            make.bottom.equalTo(safeAreaLayoutGuide.snp.bottom).offset(-102)
+        }
+        
+        // ScrollView Content View 설정
+        mainScrollView.addSubview(contentView)
+        contentView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+            make.width.equalToSuperview()
+        }
+        
+        // ScrollView 내부 콘텐츠: 이미지 뷰
+        contentView.addSubview(imageScrollView)
         imageScrollView.isPagingEnabled = true
         imageScrollView.showsHorizontalScrollIndicator = false
         imageScrollView.delegate = self
         imageScrollView.snp.makeConstraints { make in
             make.top.equalToSuperview()
             make.leading.trailing.equalToSuperview()
-            make.height.equalTo(imageScrollView.snp.width).multipliedBy(297.66 / 393.0) // 비율 유지
+            make.height.equalTo(imageScrollView.snp.width).multipliedBy(297.66 / 393.0)
         }
         
-        // ScrollView 내부 콘텐츠 뷰 추가
         imageScrollView.addSubview(imageContentView)
         imageContentView.snp.makeConstraints { make in
-            make.edges.equalToSuperview() // ScrollView 내부에서 꽉 차도록 설정
-            make.height.equalToSuperview() // 세로 크기는 ScrollView와 동일
+            make.edges.equalToSuperview()
+            make.height.equalToSuperview()
         }
         
         // PageControl 설정
-        addSubview(pageControl)
+        contentView.addSubview(pageControl)
         pageControl.currentPage = 0
         pageControl.pageIndicatorTintColor = UIColor.mainBlue.withAlphaComponent(0.3)
         pageControl.currentPageIndicatorTintColor = .mainBlue
         pageControl.backgroundColor = UIColor(red: 0.749, green: 0.749, blue: 0.749, alpha: 0.44)
         pageControl.layer.cornerRadius = 12
         pageControl.snp.makeConstraints { make in
-            make.top.equalTo(imageScrollView.snp.bottom).offset(12.37)
+            make.top.equalTo(imageScrollView.snp.bottom).offset(12.34)
             make.centerX.equalToSuperview()
             make.height.equalTo(24)
         }
     }
     
+    private func setupFrames() {
+        // Profile Frame
+        profileFrame.backgroundColor = .red
+        contentView.addSubview(profileFrame)
+        profileFrame.snp.makeConstraints { make in
+            make.top.equalTo(pageControl.snp.bottom).offset(2)
+            make.leading.trailing.equalToSuperview()
+            make.height.equalTo(102)
+        }
+        
+        infoFrame.backgroundColor = .yellow
+        contentView.addSubview(infoFrame)
+        infoFrame.snp.makeConstraints { make in
+            make.top.equalTo(profileFrame.snp.bottom).offset(34)
+            make.leading.trailing.equalToSuperview()
+            make.height.equalTo(754)
+        }
+        
+        walkInfoFrame.backgroundColor = UIColor.gray100
+        walkInfoFrame.layer.cornerRadius = 20
+        infoFrame.addSubview(walkInfoFrame)
+        walkInfoFrame.snp.makeConstraints { make in
+            make.top.equalTo(infoFrame.snp.top)
+            make.centerX.equalToSuperview()
+            make.width.equalTo(353)
+            make.height.equalTo(226)
+        }
+        
+        relatedInfoFrame.backgroundColor = UIColor.gray100
+        relatedInfoFrame.layer.cornerRadius = 20
+        infoFrame.addSubview(relatedInfoFrame)
+        relatedInfoFrame.snp.makeConstraints { make in
+            make.top.equalTo(walkInfoFrame.snp.bottom).offset(16)
+            make.centerX.equalToSuperview()
+            make.width.equalTo(353)
+            make.height.equalTo(324)
+        }
+        
+        ownerInfoFrame.backgroundColor = UIColor.gray100
+        ownerInfoFrame.layer.cornerRadius = 20
+        infoFrame.addSubview(ownerInfoFrame)
+        ownerInfoFrame.snp.makeConstraints { make in
+            make.top.equalTo(relatedInfoFrame.snp.bottom).offset(16)
+            make.centerX.equalToSuperview()
+            make.width.equalTo(353)
+            make.height.equalTo(172)
+        }
+        
+        contentView.snp.makeConstraints { make in
+            make.bottom.equalTo(infoFrame.snp.bottom).offset(20)
+        }
+        
+        addSubview(buttonFrame)
+        buttonFrame.backgroundColor = .blue
+        buttonFrame.snp.makeConstraints { make in
+            make.bottom.equalTo(safeAreaLayoutGuide.snp.bottom)
+            make.centerX.equalToSuperview()
+            make.width.equalToSuperview()
+            make.height.equalTo(102)
+        }
+    }
+    
     // MARK: - Public Methods
     func configureImages(with imageNames: [String]) {
-        // 기존 이미지 제거
         imageViews.forEach { $0.removeFromSuperview() }
         imageViews = []
         
-        // 화면의 실제 너비 가져오기
         let screenWidth = UIScreen.main.bounds.width
         
-        // 이미지 추가
         for (index, imageName) in imageNames.enumerated() {
             let imageView = UIImageView()
             imageView.image = UIImage(named: imageName) ?? UIImage(named: "defaultDogImage")
@@ -81,21 +158,18 @@ class MatchingDogInformationView: UIView, UIScrollViewDelegate {
             imageContentView.addSubview(imageView)
             imageViews.append(imageView)
             
-            // Auto Layout 설정
             imageView.snp.makeConstraints { make in
                 make.top.bottom.equalToSuperview()
-                make.width.equalTo(screenWidth) // 화면 너비에 맞추기
-                make.height.equalTo(imageScrollView.snp.height) // ScrollView의 높이에 맞추기
+                make.width.equalTo(screenWidth)
+                make.height.equalTo(imageScrollView.snp.height)
                 make.leading.equalToSuperview().offset(CGFloat(index) * screenWidth)
             }
         }
         
-        // ScrollView Content Size 설정
         imageContentView.snp.makeConstraints { make in
-            make.width.equalTo(screenWidth * CGFloat(imageNames.count)) // 총 이미지 너비
+            make.width.equalTo(screenWidth * CGFloat(imageNames.count))
         }
         
-        // PageControl 설정
         pageControl.numberOfPages = imageNames.count
     }
     
