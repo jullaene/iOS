90046acf68ed9ecd53db738368d1b9ffa14eb594
hoyeon <fnfn0901@sookmyung.ac.kr>
2024-11-10 22:15:00 +0900

    feat: MatchingDogInformationViewController 이미지 슬라이더 및 PageControl 구현
    - MatchingDogInformationView에 이미지 슬라이더 추가
      - UIScrollView와 UIPageControl을 활용하여 슬라이더 구현
      - 기기 화면 너비에 맞춰 비율(297.66:393)을 유지하도록 수정
      - 아이폰 미니 등 다양한 화면 크기에서 정상적으로 표시되도록 동적 레이아웃 적용
    - PageControl 디자인 변경
      - pageIndicatorTintColor를 .mainBlue 30% 투명도로 설정
      - currentPageIndicatorTintColor를 불투명한 .mainBlue로 설정
    - MatchingDogInformationViewController에서 이미지 슬라이더 데이터 연동
      - configureImages 메서드를 통해 여러 이미지를 전달받아 슬라이더 구성

--- walkmong/walkmong/MatchingApplyProcedure/Views/MatchingDogInformationView.swift
+++ walkmong/walkmong/MatchingApplyProcedure/Views/MatchingDogInformationView.swift
@@ -8,13 +8,13 @@
 import UIKit
 import SnapKit
 
-class MatchingDogInformationView: UIView {
+class MatchingDogInformationView: UIView, UIScrollViewDelegate {
     
     // MARK: - UI Components
-    private let nameLabel = UILabel()
-    private let infoLabel = UILabel()
-    private let imageView = UIImageView()
-    private let emptyLabel = UILabel()
+    private let imageScrollView = UIScrollView()
+    private let imageContentView = UIView() // 콘텐츠를 담을 컨테이너 뷰
+    private let pageControl = UIPageControl()
+    private var imageViews: [UIImageView] = []
     
     // MARK: - Initializer
     override init(frame: CGRect) {
@@ -31,63 +31,77 @@ class MatchingDogInformationView: UIView {
     private func setupView() {
         backgroundColor = .white
         
-        // Empty Label
-        emptyLabel.text = "강아지 정보가 없습니다."
-        emptyLabel.textColor = .gray
-        emptyLabel.textAlignment = .center
-        emptyLabel.isHidden = true
-        addSubview(emptyLabel)
-        emptyLabel.snp.makeConstraints { make in
-            make.center.equalToSuperview()
+        // ScrollView 설정
+        addSubview(imageScrollView)
+        imageScrollView.isPagingEnabled = true
+        imageScrollView.showsHorizontalScrollIndicator = false
+        imageScrollView.delegate = self
+        imageScrollView.snp.makeConstraints { make in
+            make.top.equalToSuperview()
+            make.leading.trailing.equalToSuperview()
+            make.height.equalTo(imageScrollView.snp.width).multipliedBy(297.66 / 393.0) // 비율 유지
         }
         
-        // Profile Image View
-        imageView.contentMode = .scaleAspectFit
-        addSubview(imageView)
-        imageView.snp.makeConstraints { make in
-            make.top.equalToSuperview().offset(100) // Layout will be adjusted by the controller
-            make.centerX.equalToSuperview()
-            make.width.height.equalTo(120)
+        // ScrollView 내부 콘텐츠 뷰 추가
+        imageScrollView.addSubview(imageContentView)
+        imageContentView.snp.makeConstraints { make in
+            make.edges.equalToSuperview() // ScrollView 내부에서 꽉 차도록 설정
+            make.height.equalToSuperview() // 세로 크기는 ScrollView와 동일
         }
         
-        // Name Label
-        nameLabel.font = UIFont.boldSystemFont(ofSize: 24)
-        nameLabel.textColor = .black
-        nameLabel.textAlignment = .center
-        addSubview(nameLabel)
-        nameLabel.snp.makeConstraints { make in
-            make.top.equalTo(imageView.snp.bottom).offset(20)
+        // PageControl 설정
+        addSubview(pageControl)
+        pageControl.currentPage = 0
+        pageControl.pageIndicatorTintColor = UIColor.mainBlue.withAlphaComponent(0.3)
+        pageControl.currentPageIndicatorTintColor = .mainBlue
+        pageControl.backgroundColor = UIColor(red: 0.749, green: 0.749, blue: 0.749, alpha: 0.44)
+        pageControl.layer.cornerRadius = 12
+        pageControl.snp.makeConstraints { make in
+            make.top.equalTo(imageScrollView.snp.bottom).offset(12.37)
             make.centerX.equalToSuperview()
-        }
-        
-        // Info Label
-        infoLabel.font = UIFont.systemFont(ofSize: 16)
-        infoLabel.textColor = .darkGray
-        infoLabel.textAlignment = .center
-        addSubview(infoLabel)
-        infoLabel.snp.makeConstraints { make in
-            make.top.equalTo(nameLabel.snp.bottom).offset(12)
-            make.centerX.equalToSuperview()
+            make.height.equalTo(24)
         }
     }
     
     // MARK: - Public Methods
-    func configure(with data: MatchingData?) {
-        guard let data = data else {
-            emptyLabel.isHidden = false
-            imageView.isHidden = true
-            nameLabel.isHidden = true
-            infoLabel.isHidden = true
-            return
+    func configureImages(with imageNames: [String]) {
+        // 기존 이미지 제거
+        imageViews.forEach { $0.removeFromSuperview() }
+        imageViews = []
+        
+        // 화면의 실제 너비 가져오기
+        let screenWidth = UIScreen.main.bounds.width
+        
+        // 이미지 추가
+        for (index, imageName) in imageNames.enumerated() {
+            let imageView = UIImageView()
+            imageView.image = UIImage(named: imageName) ?? UIImage(named: "defaultDogImage")
+            imageView.contentMode = .scaleAspectFill
+            imageView.clipsToBounds = true
+            imageContentView.addSubview(imageView)
+            imageViews.append(imageView)
+            
+            // Auto Layout 설정
+            imageView.snp.makeConstraints { make in
+                make.top.bottom.equalToSuperview()
+                make.width.equalTo(screenWidth) // 화면 너비에 맞추기
+                make.height.equalTo(imageScrollView.snp.height) // ScrollView의 높이에 맞추기
+                make.leading.equalToSuperview().offset(CGFloat(index) * screenWidth)
+            }
         }
         
-        emptyLabel.isHidden = true
-        imageView.isHidden = false
-        nameLabel.isHidden = false
-        infoLabel.isHidden = false
+        // ScrollView Content Size 설정
+        imageContentView.snp.makeConstraints { make in
+            make.width.equalTo(screenWidth * CGFloat(imageNames.count)) // 총 이미지 너비
+        }
         
-        imageView.image = UIImage(named: data.dogProfile) ?? UIImage(named: "defaultDogImage")
-        nameLabel.text = data.dogName
-        infoLabel.text = "나이: \(data.dogAge)살 / 품종: \(data.breed)"
+        // PageControl 설정
+        pageControl.numberOfPages = imageNames.count
+    }
+    
+    // MARK: - UIScrollViewDelegate
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        let pageIndex = round(scrollView.contentOffset.x / UIScreen.main.bounds.width)
+        pageControl.currentPage = Int(pageIndex)
     }
 }
