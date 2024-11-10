071eda886483a5fa1ecc2815cb7f91947bb17153
woongaaaa <woonge0930@naver.com>
2024-11-10 16:12:26 +0900

    design: 만남장소 검색 UI 구현

--- walkmong/walkmong/MatchingApplyProcedure/Controllers/MatchingApplyPlaceSearchViewController.swift
+++ walkmong/walkmong/MatchingApplyProcedure/Controllers/MatchingApplyPlaceSearchViewController.swift
@@ -7,23 +7,58 @@
 
 import UIKit
 
-class MatchingApplyPlaceSearchViewController: UIViewController {
-
+final class MatchingApplyPlaceSearchViewController: UIViewController {
+    
+    let placeSearchView = walkmong.MatchingApplyPlaceSearchView()
+    var placeSearchResult: [String] = ["검색결과1","검색결과2","검색결과3","검색결과4","검색결과5","검색결과6","검색결과7","검색결과8","검색결과9","검색결과10","검색결과11","검색결과12"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        dismissKeyboardOnTap()
+        addCustomNavigationBar(titleText: "만남장소", showLeftBarButton: true, showCloseBarButton: false, showRefreshBarButton: false)
+        setUpViews()
+        setConstraints()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func setUpViews(){
+        self.view.backgroundColor = .white
+        self.view.addSubview(placeSearchView)
+        placeSearchView.delegate = self
+        placeSearchView.placeSearchResults = placeSearchResult
     }
-    */
-
+    
+    private func setConstraints(){
+        placeSearchView.snp.makeConstraints{ make in
+            make.top.equalToSuperview().offset(121)
+            make.horizontalEdges.equalToSuperview()
+            make.bottom.equalToSuperview()
+        }
+    }
+    
+    private func placeSearchAPI(_ keyword: String) -> [String]{
+        return []
+    }
 }
+
+extension MatchingApplyPlaceSearchViewController: MatchingApplyPlaceSearchViewDelegate{
+    
+    func matchingApplyPlaceSearchView(_ textField: UITextField, willSearchKeywords keyword: String) {
+        DispatchQueue.global(qos: .background).async {
+            // 비동기적으로 네트워크 요청을 보냄
+            self.placeSearchResult = self.placeSearchAPI(keyword)
+            // 메인 스레드로 돌아와 UI 업데이트
+            DispatchQueue.main.async {
+                textField.endEditing(true)
+                self.placeSearchView.setNeedsLayout()
+                self.placeSearchView.layoutIfNeeded()
+            }
+        }
+    }
+    
+    func matchingApplyPlaceSearchView(_ collectionView: UICollectionView, didSelectPlaceSearchResultAt indexPath: IndexPath) {
+        // 터치 이벤트 처리
+        let nextVC = MatchingApplyMapViewController()
+        self.navigationController?.pushViewController(nextVC, animated: true)
+    }
+}
+
