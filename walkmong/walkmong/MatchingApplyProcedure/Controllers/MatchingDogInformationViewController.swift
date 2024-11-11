0beb60147be0c0845785e344fedace8d3d40d2e6
hoyeon <fnfn0901@sookmyung.ac.kr>
2024-11-11 20:55:11 +0900

    feat: 산책 지원하기 버튼  클릭 시 다음 화면으로 이동

--- walkmong/walkmong/MatchingApplyProcedure/Controllers/MatchingDogInformationViewController.swift
+++ walkmong/walkmong/MatchingApplyProcedure/Controllers/MatchingDogInformationViewController.swift
@@ -1,10 +1,3 @@
-//
-//  MatchingDogInformationViewController.swift
-//  walkmong
-//
-//  Created by 황채웅 on 11/3/24.
-//
-
 import UIKit
 import SnapKit
 
@@ -17,29 +10,20 @@ class MatchingDogInformationViewController: BaseViewController {
     // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        // 커스텀 네비게이션 바 설정
         setupCustomNavigationBar()
-        
-        // UI 설정
         setupUI()
         if let data = matchingData {
-            // 여러 이미지 설정 (슬라이더)
             dogInfoView.configureImages(with: [data.dogProfile, "sampleImage"])
         }
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        
-        // 기본 탭바 숨기기
         tabBarController?.tabBar.isHidden = true
     }
     
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
-        
-        // 기본 탭바 다시 표시
         tabBarController?.tabBar.isHidden = false
     }
     
@@ -52,7 +36,6 @@ class MatchingDogInformationViewController: BaseViewController {
     private func setupUI() {
         view.backgroundColor = .white
         view.addSubview(dogInfoView)
-        
         dogInfoView.snp.makeConstraints { make in
             make.top.equalTo(customNavigationBar.snp.bottom)
             make.leading.trailing.bottom.equalToSuperview()
@@ -62,6 +45,12 @@ class MatchingDogInformationViewController: BaseViewController {
     private func setupCustomNavigationBar() {
         customNavigationBar.setTitle("")
         customNavigationBar.addBackButtonAction(target: self, action: #selector(customBackButtonTapped))
+    }
+    
+    // MARK: - 화면 전환 메서드
+    func navigateToMatchingApplyDetailSelect() {
+        let detailViewController = MatchingApplyDetailSelectViewController()
+        navigationController?.pushViewController(detailViewController, animated: true)
     }
     
     // MARK: - Actions
