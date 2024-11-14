5f658a3033ca52fe233275be213d32421147eda2
woongaaaa <woonge0930@naver.com>
2024-11-15 04:09:02 +0900

    design: 산책 지원 - 프로필 정보 뷰 모듈화

---
+++ walkmong/walkmong/MatchingApplyProcedure/Views/MatchingProfileInformationView.swift
@@ -0,0 +1,116 @@
+//
+//  MatchingProfileInformationView.swift
+//  walkmong
+//
+//  Created by 황채웅 on 11/14/24.
+//
+
+import UIKit
+
+class BaseProfileInformationView: UIView {
+    
+    internal let profileTitleLabel = SmallTitleLabel(text: "반려견 정보")
+    
+    internal let profileImageview: UIImageView = {
+        let imageview = UIImageView()
+        imageview.image = .matchingApplyProcedureIcon
+        imageview.contentMode = .scaleAspectFit
+        imageview.layer.cornerRadius = 41
+        return imageview
+    }()
+        
+    internal let nameLabel = SmallTitleLabel(text: "봄별이")
+    
+    internal let profileInformationLabel = SmallMainParagraphLabel(text: "소형견 · 말티즈 · 4kg")
+    
+    internal let locationImageView: UIImageView = {
+        let imageview = UIImageView()
+        imageview.image = .locationIcon
+        imageview.contentMode = .scaleAspectFit
+        return imageview
+    }()
+    
+    internal let locationLabel = SmallMainParagraphLabel(text: "장소", textColor: .gray400)
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        addSubView()
+        setConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func addSubView(){
+        self.addSubviews(profileTitleLabel, profileImageview, nameLabel, profileInformationLabel, locationImageView, locationLabel)
+    }
+    
+    private func setConstraints(){
+        profileTitleLabel.snp.makeConstraints { make in
+            make.leading.equalToSuperview().offset(20)
+            make.top.equalToSuperview().offset(24)
+        }
+        profileImageview.snp.makeConstraints { make in
+            make.leading.equalToSuperview().offset(20)
+            make.top.equalTo(profileTitleLabel.snp.bottom).offset(16)
+            make.width.height.equalTo(82)
+        }
+        nameLabel.snp.makeConstraints { make in
+            make.leading.equalTo(profileImageview.snp.trailing).offset(12)
+            make.top.equalTo(profileImageview.snp.top)
+        }
+        locationLabel.snp.makeConstraints { make in
+            make.leading.equalTo(profileImageview.snp.trailing).offset(32)
+            make.top.equalTo(nameLabel.snp.bottomMargin).offset(8)
+        }
+        locationImageView.snp.makeConstraints { make in
+            make.centerY.equalTo(locationLabel.snp.centerY)
+            make.trailing.equalTo(locationLabel.snp.leading).inset(4)
+            make.width.height.equalTo(14)
+        }
+        profileInformationLabel.snp.makeConstraints { make in
+            make.centerY.equalTo(nameLabel.snp.centerY)
+            make.leading.equalTo(nameLabel.snp.trailing).offset(44)
+        }
+    }
+    func configure(name: String, informationText: String, location: String, profileImage: UIImage) {
+        profileImageview.image = .matchingApplyProcedureIcon // 임시 이미지
+        nameLabel.text = name
+        profileInformationLabel.text = informationText
+        locationLabel.text = location
+    }
+}
+
+
+class DogProfileInformationView: BaseProfileInformationView{
+    
+    private let genderIcon: UIImageView = {
+        let imageview = UIImageView()
+        imageview.contentMode = .scaleAspectFit
+        return imageview
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.addSubview(genderIcon)
+        genderIcon.snp.makeConstraints { make in
+            make.top.equalToSuperview().offset(74)
+            make.leading.equalToSuperview().offset(173.5)
+            make.width.equalTo(9)
+            make.height.equalTo(16)
+        }
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func configure(isFemale: Bool, name: String, informationText: String, location: String, profileImage: UIImage) {
+        genderIcon.image = isFemale ? .femaleIcon : .maleIcon
+        profileImageview.image = .matchingApplyProcedureIcon // 임시 이미지
+        nameLabel.text = name
+        profileInformationLabel.text = informationText
+        locationLabel.text = location
+    }
+}
