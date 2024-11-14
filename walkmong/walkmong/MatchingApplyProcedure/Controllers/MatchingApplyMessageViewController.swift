c4e01eaeee40d10feb349453697dd6cbaa5a0e3b
woongaaaa <woonge0930@naver.com>
2024-11-14 18:01:39 +0900

    feat: 산책 지원 - 전달 메세지 UI 구현

--- walkmong/walkmong/MatchingApplyProcedure/Controllers/MatchingApplyMessageViewController.swift
+++ walkmong/walkmong/MatchingApplyProcedure/Controllers/MatchingApplyMessageViewController.swift
@@ -8,22 +8,38 @@
 import UIKit
 
 class MatchingApplyMessageViewController: UIViewController {
+    
+    let messageView = MatchingApplyMessageView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        self.view.backgroundColor = .white
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(self.didTapTextView(_:)))
+        view.addGestureRecognizer(tapGesture)
+        addCustomNavigationBar(titleText: "산책 지원하기", showLeftBackButton: true, showLeftCloseButton: false, showRightCloseButton: false, showRightRefreshButton: false)
+        addProgressBar(currentStep: 2, totalSteps: 3)
+        addSubviews()
+        setConstraints()
+        messageView.delegate = self
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func addSubviews(){
+        self.view.addSubview(messageView)
     }
-    */
-
+    
+    private func setConstraints(){
+        messageView.snp.makeConstraints{ make in
+            make.top.equalToSuperview().offset(152)
+            make.bottom.horizontalEdges.equalToSuperview()
+        }
+    }
+    @objc private func didTapTextView(_ sender: Any) {
+        view.endEditing(true)
+    }
+}
+extension MatchingApplyMessageViewController: MatchingApplyMessageViewDelegate {
+    func didTapNextButton() {
+        let nextVC = MatchingApplyFinalViewController()
+        self.navigationController?.pushViewController(nextVC, animated: true)
+    }
 }
