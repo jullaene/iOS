5795c2f63cf3056c776162740711c7f1b05fdabc
hoyeon <fnfn0901@sookmyung.ac.kr>
2024-11-15 15:36:27 +0900

    refactor: 백신 뷰 리팩토링

--- walkmong/walkmong/Mypage/Views/VaccinationView.swift
+++ walkmong/walkmong/Mypage/Views/VaccinationView.swift
@@ -19,41 +19,59 @@ class VaccinationView: UIView {
         static let iconSize: CGFloat = 20
         static let spacing: CGFloat = 12
         static let iconSpacing: CGFloat = 6
+
         static let titleFont = UIFont(name: "Pretendard-SemiBold", size: 20)!
         static let descriptionFont = UIFont(name: "Pretendard-SemiBold", size: 16)!
-        
+
         // Colors
-        static let completedBackgroundColor = UIColor(red: 0.913, green: 1, blue: 0.948, alpha: 1)
-        static let incompleteBackgroundColor = UIColor(red: 0.957, green: 0.929, blue: 0.902, alpha: 1)
-        static let completedTextColor = UIColor.mainGreen
-        static let incompleteTextColor = UIColor(red: 0.929, green: 0.529, blue: 0.471, alpha: 1)
+        struct Colors {
+            static let completedBackground = UIColor(red: 0.913, green: 1, blue: 0.948, alpha: 1)
+            static let incompleteBackground = UIColor(red: 0.957, green: 0.929, blue: 0.902, alpha: 1)
+            static let completedText = UIColor.mainGreen
+            static let incompleteText = UIColor(red: 0.929, green: 0.529, blue: 0.471, alpha: 1)
+            static let titleText = UIColor.mainBlack
+        }
 
         // Icon Names
-        static let completedIcon = "VaccinationCompletedIcon"
-        static let incompleteIcon = "VaccinationIncompleteIcon"
+        struct Icons {
+            static let completed = "VaccinationCompletedIcon"
+            static let incomplete = "VaccinationIncompleteIcon"
+        }
+
+        // Texts
+        struct Texts {
+            static let title = "강아지 예방접종"
+            static let completed = "광견병 예방접종을 완료했어요."
+            static let incomplete = "광견병 예방접종이 필요합니다."
+        }
     }
 
     // MARK: - UI Components
-    private let titleLabel = VaccinationView.createLabel(
-        text: "강아지 예방접종",
-        font: Constants.titleFont,
-        color: UIColor.mainBlack
-    )
-    
+    private let titleLabel: UILabel = {
+        return createLabel(
+            text: Constants.Texts.title,
+            font: Constants.titleFont,
+            color: Constants.Colors.titleText
+        )
+    }()
+
     private let vaccinationFrame: UIView = {
         let view = UIView()
         view.layer.cornerRadius = Constants.cornerRadius
-        view.backgroundColor = Constants.completedBackgroundColor
         return view
     }()
 
-    private let iconView = VaccinationView.createImageView(iconName: Constants.completedIcon)
-    
-    private let descriptionLabel = VaccinationView.createLabel(
-        text: "광견병 예방접종을 완료했어요.",
-        font: Constants.descriptionFont,
-        color: Constants.completedTextColor
-    )
+    private let iconView: UIImageView = {
+        return createImageView(iconName: Constants.Icons.completed)
+    }()
+
+    private let descriptionLabel: UILabel = {
+        return createLabel(
+            text: Constants.Texts.completed,
+            font: Constants.descriptionFont,
+            color: Constants.Colors.completedText
+        )
+    }()
 
     // MARK: - Initializer
     override init(frame: CGRect) {
@@ -77,27 +95,23 @@ class VaccinationView: UIView {
     }
 
     private func setupLayout() {
-        // Title Label Layout
         titleLabel.snp.makeConstraints { make in
             make.top.leading.trailing.equalToSuperview()
             make.height.equalTo(Constants.titleHeight)
         }
 
-        // Vaccination Frame Layout
         vaccinationFrame.snp.makeConstraints { make in
             make.top.equalTo(titleLabel.snp.bottom).offset(Constants.spacing)
             make.leading.trailing.equalToSuperview()
             make.height.equalTo(Constants.vaccinationFrameHeight)
         }
 
-        // Icon View Layout
         iconView.snp.makeConstraints { make in
             make.leading.equalToSuperview().offset(Constants.spacing)
             make.centerY.equalToSuperview()
             make.size.equalTo(Constants.iconSize)
         }
 
-        // Description Label Layout
         descriptionLabel.snp.makeConstraints { make in
             make.leading.equalTo(iconView.snp.trailing).offset(Constants.iconSpacing)
             make.centerY.equalToSuperview()
@@ -108,10 +122,21 @@ class VaccinationView: UIView {
     // MARK: - Public Methods
     func updateVaccinationStatus(rabiesYn: String) {
         let isVaccinated = rabiesYn == "Y"
-        vaccinationFrame.backgroundColor = isVaccinated ? Constants.completedBackgroundColor : Constants.incompleteBackgroundColor
-        iconView.image = UIImage(named: isVaccinated ? Constants.completedIcon : Constants.incompleteIcon)
-        descriptionLabel.textColor = isVaccinated ? Constants.completedTextColor : Constants.incompleteTextColor
-        descriptionLabel.text = isVaccinated ? "광견병 예방접종을 완료했어요." : "광견병 예방접종이 필요합니다."
+        vaccinationFrame.backgroundColor = isVaccinated
+            ? Constants.Colors.completedBackground
+            : Constants.Colors.incompleteBackground
+
+        iconView.image = UIImage(named: isVaccinated
+            ? Constants.Icons.completed
+            : Constants.Icons.incomplete)
+
+        descriptionLabel.textColor = isVaccinated
+            ? Constants.Colors.completedText
+            : Constants.Colors.incompleteText
+
+        descriptionLabel.text = isVaccinated
+            ? Constants.Texts.completed
+            : Constants.Texts.incomplete
     }
 
     // MARK: - Helper Methods
@@ -119,6 +144,7 @@ class VaccinationView: UIView {
         let label = UILabel()
         let paragraphStyle = NSMutableParagraphStyle()
         paragraphStyle.lineHeightMultiple = 1.17
+
         label.attributedText = NSAttributedString(
             string: text,
             attributes: [
