aa1c88542c5db81d9f897ad75ab0bf343a405af0
woongaaaa <woonge0930@naver.com>
2024-11-16 00:58:07 +0900

    feat: GeocodingAPI 호출 함수 async-await 구조로 변경

---
+++ walkmong/walkmong/MatchingApplyProcedure/Models/MatchingApplyPlaceSearchModel.swift
@@ -0,0 +1,12 @@
+//
+//  MatchingApplyPlaceSearchModel.swift
+//  walkmong
+//
+//  Created by 황채웅 on 11/16/24.
+//
+
+import Foundation
+
+struct MatchingApplyPlaceSearchModel: Codable{
+    var result: Geocoding
+}
