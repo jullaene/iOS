958cc75133f54c46dae78bb96b86e2064d4302c0
hoyeon <fnfn0901@sookmyung.ac.kr>
2024-11-28 11:40:48 +0900

    fix: 컴포넌트에 맞게 버튼 스타일 수정

--- walkmong/walkmong/Extension/UIButton+.swift
+++ walkmong/walkmong/Extension/UIButton+.swift
@@ -11,81 +11,75 @@ extension UIButton {
     enum ButtonStyle {
         case light
         case dark
-        case rectangular
+        case profile
     }
     
-    enum ButtonSize {
+    enum ButtonCategory {
         case large
-        case medium
-        case small1
-        case small2
+        case largeSelection
+        case smallSelection
+        case homeFilter
     }
     
-    static func createStyledButton(style: ButtonStyle, size: ButtonSize, title: String) -> UIButton {
+    static func createStyledButton(type: ButtonCategory, style: ButtonStyle, title: String) -> UIButton {
         let button = UIButton(type: .system)
         button.translatesAutoresizingMaskIntoConstraints = false
         
-        // 배경색 설정
-        switch style {
-        case .light:
-            button.backgroundColor = UIColor.gray100
-            if size == .large {
-                button.setTitleColor(.white, for: .normal)
-            } else {
-                button.setTitleColor(UIColor.gray400, for: .normal)
-            }
-        case .dark:
-            button.backgroundColor = UIColor.gray600
-            button.setTitleColor(UIColor.white, for: .normal)
-        case .rectangular:
-            button.backgroundColor = UIColor.mainBlue
-            button.setTitleColor(UIColor.white, for: .normal)
-        }
-        
-        // 텍스트 설정
-        button.setTitle(title, for: .normal)
+        // 텍스트 폰트 설정
         let font: UIFont = {
-            switch style {
-            case .rectangular:
+            switch type {
+            case .large, .homeFilter:
                 return UIFont(name: "Pretendard-Bold", size: 16)!
             default:
-                return UIFont(name: "Pretendard-SemiBold", size: 16)!
+                return UIFont(name: "Pretendard-SemiBold", size: 14)!
             }
         }()
         button.titleLabel?.font = font
         button.titleLabel?.textAlignment = .center
         
-        // 텍스트 너비 계산
-        let textWidth: CGFloat = {
-            let attributes: [NSAttributedString.Key: Any] = [.font: font]
-            return (title as NSString).size(withAttributes: attributes).width
-        }()
-        
-        // 코너 반경 설정
-        if size == .small2 {
-            button.layer.cornerRadius = 18.5
-        } else if style == .rectangular {
-            button.layer.cornerRadius = 10
-        } else {
+        // 배경색 및 텍스트 색상 설정
+        switch type {
+        case .large:
             button.layer.cornerRadius = 15
+            button.backgroundColor = style == .light ? .gray300 : .gray600
+            button.setTitleColor(style == .light ? .white : .gray100, for: .normal)
+        case .largeSelection:
+            button.layer.cornerRadius = 10
+            button.backgroundColor = style == .light ? .gray100 : .mainBlue
+            button.setTitleColor(style == .light ? .gray500 : .gray100, for: .normal)
+        case .smallSelection:
+            button.layer.cornerRadius = 10
+            button.backgroundColor = style == .light ? .gray100 : .mainBlue
+            button.setTitleColor(style == .light ? .gray500 : .white, for: .normal)
+        case .homeFilter:
+            button.layer.cornerRadius = 15
+            button.backgroundColor = style == .light ? .gray100 : .gray600
+            button.setTitleColor(style == .light ? .gray500 : .white, for: .normal)
         }
+        
+        // 텍스트 설정
+        button.setTitle(title, for: .normal)
         
         // 크기 설정
         let width: CGFloat
         let height: CGFloat
-        switch size {
+        switch type {
         case .large:
             width = 363
             height = 54
-        case .medium:
-            width = 251
-            height = 54
-        case .small1:
-            width = 93
-            height = 53
-        case .small2:
-            width = style == .rectangular ? 74 : textWidth + 32
-            height = style == .rectangular ? 34 : 38
+        case .largeSelection:
+            width = 361
+            height = 46
+        case .smallSelection:
+            width = 73
+            height = 38
+        case .homeFilter:
+            let textWidth: CGFloat = {
+                let attributes: [NSAttributedString.Key: Any] = [.font: font]
+                return (title as NSString).size(withAttributes: attributes).width
+            }()
+            width = textWidth + 32 // 좌우 패딩 16씩
+            height = font.lineHeight + 16 // 상하 패딩 8씩
         }
         
         NSLayoutConstraint.activate([
