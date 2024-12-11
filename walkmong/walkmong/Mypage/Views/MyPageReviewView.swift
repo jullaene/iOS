3abed51c2fa1ff75f2235bf95cb1cdf1738b6922
hoyeon <fnfn0901@sookmyung.ac.kr>
2024-12-11 15:54:50 +0900

    design: 받은 후기 텍스트 뷰 구현

--- walkmong/walkmong/Mypage/Views/MyPageReviewView.swift
+++ walkmong/walkmong/Mypage/Views/MyPageReviewView.swift
@@ -6,8 +6,34 @@
 //
 
 import UIKit
+import SnapKit
 
 class MyPageReviewView: UIView {
+    
+    private let walkerReviewTitle = ReviewTitleView(title: "받은 산책자 후기", count: 5)
+    private let ownerReviewTitle = ReviewTitleView(title: "받은 반려인 후기", count: 3)
+    
+    private let userRatingView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .gray100
+        view.layer.cornerRadius = 15
+        return view
+    }()
+    
+    private let keywordView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .gray100
+        view.layer.cornerRadius = 15
+        return view
+    }()
+    
+    private let ownerReviewView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .gray100
+        view.layer.cornerRadius = 15
+        return view
+    }()
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         setupView()
@@ -18,6 +44,98 @@ class MyPageReviewView: UIView {
     }
     
     private func setupView() {
-        self.backgroundColor = .green
+        addSubview(walkerReviewTitle)
+        addSubview(userRatingView)
+        addSubview(keywordView)
+        addSubview(ownerReviewTitle)
+        addSubview(ownerReviewView)
+        
+        setupConstraints()
+    }
+    
+    private func setupConstraints() {
+        walkerReviewTitle.snp.makeConstraints { make in
+            make.top.equalToSuperview().offset(20)
+            make.leading.trailing.equalToSuperview()
+            make.height.equalTo(34)
+        }
+        
+        userRatingView.snp.makeConstraints { make in
+            make.top.equalTo(walkerReviewTitle.snp.bottom).offset(20)
+            make.leading.trailing.equalToSuperview()
+            make.height.equalTo(340)
+        }
+        
+        keywordView.snp.makeConstraints { make in
+            make.top.equalTo(userRatingView.snp.bottom).offset(20)
+            make.leading.trailing.equalToSuperview()
+            make.height.equalTo(305)
+        }
+        
+        ownerReviewTitle.snp.makeConstraints { make in
+            make.top.equalTo(keywordView.snp.bottom).offset(20)
+            make.leading.trailing.equalToSuperview()
+            make.height.equalTo(34)
+        }
+        
+        ownerReviewView.snp.makeConstraints { make in
+            make.top.equalTo(ownerReviewTitle.snp.bottom).offset(20)
+            make.leading.trailing.equalToSuperview()
+            make.height.equalTo(128)
+        }
+        
+        ownerReviewView.snp.makeConstraints { make in
+            make.bottom.lessThanOrEqualToSuperview().inset(40)
+        }
+    }
+}
+
+class ReviewTitleView: UIView {
+    private let titleLabel: MiddleTitleLabel
+    private let countLabel: MainHighlightParagraphLabel = {
+        let label = MainHighlightParagraphLabel(text: "0개", textColor: .gray600)
+        return label
+    }()
+    private let arrowImageView: UIImageView = {
+        let imageView = UIImageView(image: UIImage(named: "MyPageReviewArrow"))
+        imageView.contentMode = .scaleAspectFit
+        return imageView
+    }()
+    
+    init(title: String, count: Int) {
+        self.titleLabel = MiddleTitleLabel(text: title)
+        super.init(frame: .zero)
+        countLabel.text = "\(count)개"
+        setupView()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupView() {
+        addSubview(titleLabel)
+        addSubview(countLabel)
+        addSubview(arrowImageView)
+        
+        setupConstraints()
+    }
+    
+    private func setupConstraints() {
+        titleLabel.snp.makeConstraints { make in
+            make.leading.equalToSuperview()
+            make.centerY.equalToSuperview()
+        }
+        
+        countLabel.snp.makeConstraints { make in
+            make.leading.equalTo(titleLabel.snp.trailing).offset(4)
+            make.centerY.equalToSuperview()
+        }
+        
+        arrowImageView.snp.makeConstraints { make in
+            make.trailing.equalToSuperview().offset(-4)
+            make.centerY.equalToSuperview()
+            make.width.height.equalTo(20)
+        }
     }
 }
