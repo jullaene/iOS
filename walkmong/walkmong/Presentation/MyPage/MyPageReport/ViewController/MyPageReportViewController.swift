de1cb962afc93fd06cd450e9b52a542dca8dad04
hoyeon <fnfn0901@sookmyung.ac.kr>
2024-12-17 18:36:08 +0900

    feat: MyPageReportView에 titleLabel 추가

--- walkmong/walkmong/Presentation/MyPage/MyPageReport/ViewController/MyPageReportViewController.swift
+++ walkmong/walkmong/Presentation/MyPage/MyPageReport/ViewController/MyPageReportViewController.swift
@@ -10,32 +10,17 @@ import SnapKit
 
 class MyPageReportViewController: UIViewController {
     
-    // MARK: - Properties
-    private let myPageReportView = MyPageReportView()
+    private lazy var myPageReportView = MyPageReportView(viewController: self)
     
-    // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
         setupView()
-        setupNavigationBar()
     }
     
-    // MARK: - View 설정
     private func setupView() {
         view.addSubview(myPageReportView)
         myPageReportView.snp.makeConstraints { make in
             make.edges.equalToSuperview()
         }
-    }
-    
-    // MARK: - NavigationBar 설정
-    private func setupNavigationBar() {
-        addCustomNavigationBar(
-            titleText: "산책 후기 신고하기",
-            showLeftBackButton: false,
-            showLeftCloseButton: false,
-            showRightCloseButton: true,
-            showRightRefreshButton: false
-        )
     }
 }
--- walkmong/walkmong/Presentation/MyPage/MyPageReport/Views/MyPageReportView.swift
+++ walkmong/walkmong/Presentation/MyPage/MyPageReport/Views/MyPageReportView.swift
@@ -5,14 +5,29 @@
 //  Created by 신호연 on 12/17/24.
 //
 
+//
+//  MyPageReportView.swift
+//  walkmong
+//
+//  Created by 신호연 on 12/17/24.
+//
+
 import UIKit
 import SnapKit
 
 class MyPageReportView: UIView {
     
+    // MARK: - UI Components
+    private let titleLabel = MiddleTitleLabel(text: "신고 사유를 선택해주세요.", textColor: .gray600)
+    private var navigationBar: UIView?
+    private weak var viewController: UIViewController?
+
     // MARK: - Initializer
-    override init(frame: CGRect) {
-        super.init(frame: frame)
+    init(viewController: UIViewController) {
+        self.viewController = viewController
+        super.init(frame: .zero)
+        
+        setupNavigationBar()
         setupLayout()
     }
     
@@ -20,8 +35,38 @@ class MyPageReportView: UIView {
         fatalError("init(coder:) has not been implemented")
     }
     
-    // MARK: - UI 설정
+    // MARK: - Layout 설정
     private func setupLayout() {
-        self.backgroundColor = .white
+        backgroundColor = .white
+        
+        addSubview(titleLabel)
+        if let navigationBar = navigationBar {
+            titleLabel.snp.makeConstraints { make in
+                make.top.equalTo(navigationBar.snp.bottom).offset(43)
+                make.left.equalToSuperview().offset(21)
+            }
+        }
+    }
+    
+    // MARK: - NavigationBar 설정
+    private func setupNavigationBar() {
+        guard let viewController = viewController else { return }
+        viewController.addCustomNavigationBar(
+            titleText: "산책 후기 신고하기",
+            showLeftBackButton: false,
+            showLeftCloseButton: false,
+            showRightCloseButton: true,
+            showRightRefreshButton: false
+        )
+        
+        if let navigationView = viewController.view.subviews.last {
+            navigationBar = navigationView
+            addSubview(navigationBar!)
+            navigationBar?.snp.makeConstraints { make in
+                make.left.right.equalToSuperview()
+                make.top.equalTo(safeAreaLayoutGuide.snp.top)
+                make.height.equalTo(52)
+            }
+        }
     }
 }
