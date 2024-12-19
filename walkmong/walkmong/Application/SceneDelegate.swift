e72eb21137470dc72021a561d2590f0b8000c36a
woongaaaa <woonge0930@naver.com>
2024-12-20 04:40:15 +0900

    fix: 초기 화면 설정 복구

--- walkmong/walkmong/Application/SceneDelegate.swift
+++ walkmong/walkmong/Application/SceneDelegate.swift
@@ -15,7 +15,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
 
         window = UIWindow(windowScene: windowScene)
-        let mainTabBarController = WalktalkChatViewController()
+        let mainTabBarController = MainTabBarController()
         let navigationController = UINavigationController(rootViewController: mainTabBarController)
         window?.rootViewController = navigationController
         
