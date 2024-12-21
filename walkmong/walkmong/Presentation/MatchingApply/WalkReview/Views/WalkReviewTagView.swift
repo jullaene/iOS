295fde8db5d556e7358df20e52eef778656ea4e6
hoyeon <fnfn0901@sookmyung.ac.kr>
2024-12-21 10:37:08 +0900

    feat: 태그 뷰 "+숫자" 기능 구현

--- walkmong/walkmong/Presentation/MatchingApply/WalkReview/Views/WalkReviewTagView.swift
+++ walkmong/walkmong/Presentation/MatchingApply/WalkReview/Views/WalkReviewTagView.swift
@@ -9,28 +9,78 @@ import UIKit
 import SnapKit
 
 class WalkReviewTagView: UIView {
+    // MARK: - Properties
     private var tagLabels: [UILabel] = []
     private let spacing: CGFloat = 4
-
+    private var isExpanded: Bool = false
+    private var tags: [String] = []
     private var heightConstraint: Constraint?
 
-    override func layoutSubviews() {
-        super.layoutSubviews()
-        layoutTags()
+    // MARK: - Public Methods
+    func configure(with tags: [String]) {
+        self.tags = tags
+        resetTags()
+        updateTags()
     }
 
-    func configure(with tags: [String]) {
+    // MARK: - Private Methods
+    private func resetTags() {
         tagLabels.forEach { $0.removeFromSuperview() }
-        tagLabels = []
+        tagLabels.removeAll()
+    }
 
-        tags.forEach { tagText in
+    private func updateTags() {
+        let displayedTags = getDisplayedTags()
+        
+        displayedTags.forEach { tagText in
             let tagLabel = createTagLabel(with: tagText)
             tagLabels.append(tagLabel)
             addSubview(tagLabel)
+            
+            if tagText.starts(with: "+") {
+                let tapGesture = UITapGestureRecognizer(target: self, action: #selector(expandTags))
+                tagLabel.addGestureRecognizer(tapGesture)
+                tagLabel.isUserInteractionEnabled = true
+            }
+        }
+        setNeedsLayout()
+        layoutIfNeeded()
+    }
+
+    private func getDisplayedTags() -> [String] {
+        if isExpanded || tags.count <= 1 {
+            return tags
+        } else {
+            return Array(tags.prefix(1)) + ["+\(tags.count - 1)"]
         }
     }
 
-    private func layoutTags() {
+    private func createTagLabel(with text: String) -> UILabel {
+        let label = UILabel()
+        label.text = text
+        label.textColor = .gray600
+        label.font = UIFont(name: "Pretendard-Medium", size: 12)
+        label.textAlignment = .center
+        label.backgroundColor = .gray200
+        label.layer.cornerRadius = 5
+        label.clipsToBounds = true
+        label.sizeToFit()
+        return label
+    }
+
+    @objc private func expandTags() {
+        isExpanded = true
+        resetTags()
+        updateTags()
+    }
+
+    // MARK: - Layout
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        layoutTagLabels()
+    }
+
+    private func layoutTagLabels() {
         guard !tagLabels.isEmpty else { return }
 
         var currentX: CGFloat = 0
@@ -52,26 +102,13 @@ class WalkReviewTagView: UIView {
         }
 
         let totalHeight = currentY + tagHeight
-
-        if let heightConstraint = self.heightConstraint {
-            heightConstraint.deactivate()
-            self.heightConstraint = nil
-        }
-
-        self.snp.makeConstraints { make in
-            self.heightConstraint = make.height.equalTo(totalHeight).constraint
-        }
+        updateHeightConstraint(to: totalHeight)
     }
 
-    private func createTagLabel(with text: String) -> UILabel {
-        let label = UILabel()
-        label.text = text
-        label.textColor = .gray600
-        label.font = UIFont(name: "Pretendard-Medium", size: 12)
-        label.textAlignment = .center
-        label.backgroundColor = .gray200
-        label.layer.cornerRadius = 5
-        label.clipsToBounds = true
-        return label
+    private func updateHeightConstraint(to height: CGFloat) {
+        heightConstraint?.deactivate()
+        self.snp.makeConstraints { make in
+            heightConstraint = make.height.equalTo(height).constraint
+        }
     }
 }
