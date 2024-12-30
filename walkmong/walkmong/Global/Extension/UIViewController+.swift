5c094aeab5c52d65bb76d0c164de26a7114fefbf
woongaaaa <woonge0930@naver.com>
2024-12-30 16:24:22 +0900

    feat: setupKeyboardEvent(for targetView: UIView) 구현

--- walkmong/walkmong/Global/Extension/UIViewController+.swift
+++ walkmong/walkmong/Global/Extension/UIViewController+.swift
@@ -7,43 +7,44 @@
 
 import UIKit
 
-// Extend UIViewController to dismiss keyboard when tapping outside the UITextField
 extension UIViewController {
     
-    // Dismiss the keyboard when tapping anywhere on the view
-    func dismissKeyboardOnTap() {
+    func dismissKeyboardOnTap(for view: UIView) {
         let tapGesture = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
         tapGesture.cancelsTouchesInView = false
         view.addGestureRecognizer(tapGesture)
     }
     
-    func setupKeyboardEvent() {
-        NotificationCenter.default.addObserver(self,
-                                               selector: #selector(keyboardWillShow),
-                                               name: UIResponder.keyboardWillShowNotification,
-                                               object: nil)
-        NotificationCenter.default.addObserver(self,
-                                               selector: #selector(keyboardWillHide),
-                                               name: UIResponder.keyboardWillHideNotification,
-                                               object: nil)
-    }
-    
-    @objc func keyboardWillShow(_ sender: Notification) {
-        guard let keyboardFrame = sender.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue else { return }
-        let keyboardHeight = keyboardFrame.cgRectValue.height
-        if view.frame.origin.y == 0 {
-            view.frame.origin.y -= keyboardHeight
-        }
-    }
-
-    @objc func keyboardWillHide(_ sender: Notification) {
-        if view.frame.origin.y != 0 {
-            view.frame.origin.y = 0
-        }
-    }
-    
-    // Dismiss keyboard method
     @objc private func dismissKeyboard() {
         view.endEditing(true)
     }
+    
+    func setupKeyboardEvent(for targetView: UIView) {
+        dismissKeyboardOnTap(for: targetView)
+        
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(keyboardWillShow),
+                                               name: UIResponder.keyboardWillShowNotification,
+                                               object: targetView)
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(keyboardWillHide),
+                                               name: UIResponder.keyboardWillHideNotification,
+                                               object: targetView)
+    }
+    
+    
+    @objc private func keyboardWillShow(_ sender: Notification, for targetView: UIView) {
+        guard let keyboardFrame = sender.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue else { return }
+        let keyboardHeight = keyboardFrame.cgRectValue.height
+        if targetView.frame.origin.y == 0 {
+            targetView.frame.origin.y -= keyboardHeight
+        }
+    }
+    
+    @objc private func keyboardWillHide(_ sender: Notification, for targetView: UIView) {
+        if targetView.frame.origin.y != 0 {
+            targetView.frame.origin.y = 0
+        }
+    }
+    
 }
