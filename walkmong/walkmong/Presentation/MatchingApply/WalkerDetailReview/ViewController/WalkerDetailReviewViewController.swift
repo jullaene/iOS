e9c409aa5a0264c2b1679ec5f3f7648e279d7c39
hoyeon <fnfn0901@sookmyung.ac.kr>
2025-01-01 14:49:23 +0900

    feat: WalkerDetailReviewViewController 구현 완료

--- walkmong/walkmong/Presentation/MatchingApply/WalkerDetailReview/ViewController/WalkerDetailReviewViewController.swift
+++ walkmong/walkmong/Presentation/MatchingApply/WalkerDetailReview/ViewController/WalkerDetailReviewViewController.swift
@@ -6,11 +6,42 @@
 //
 
 import UIKit
+import SnapKit
 
 final class WalkerDetailReviewViewController: UIViewController {
+
+    // MARK: - Properties
+    private let reviewPhotoView = ReviewPhotoView()
+
+    // MARK: - Life Cycle
     override func viewDidLoad() {
-    
         super.viewDidLoad()
-        
+        setupView()
+        setupNavigationBar()
+        disableBackSlideGesture()
+    }
+
+    // MARK: - Setup Methods
+    private func setupView() {
+        view.backgroundColor = .white
+        view.addSubview(reviewPhotoView)
+        reviewPhotoView.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide.snp.top).offset(52+23)
+            make.leading.trailing.equalToSuperview().inset(20)
         }
+    }
+
+    private func setupNavigationBar() {
+        addCustomNavigationBar(
+            titleText: "자세한 후기 쓰기",
+            showLeftBackButton: false,
+            showLeftCloseButton: false,
+            showRightCloseButton: false,
+            showRightRefreshButton: false
+        )
+    }
+
+    private func disableBackSlideGesture() {
+        navigationController?.interactivePopGestureRecognizer?.isEnabled = false
+    }
 }
