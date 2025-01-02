2f867d85e657442fb3d1c7c979a4b385932e5f51
woongaaaa <woonge0930@naver.com>
2025-01-02 20:55:50 +0900

    chore: 회원가입 정보 입력 UI 파일 생성

---
+++ walkmong/walkmong/Presentation/Signup/Detail/ViewController/SignupDetailViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SignupDetailViewController.swift
+//  walkmong
+//
+//  Created by 황채웅 on 1/1/25.
+//
+
+import UIKit
+
+class SignupDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
---
+++ walkmong/walkmong/Presentation/Signup/Detail/Views/SignupDetailView.swift
@@ -0,0 +1,19 @@
+//
+//  SignupDetailView.swift
+//  walkmong
+//
+//  Created by 황채웅 on 1/1/25.
+//
+
+import UIKit
+
+class SignupDetailView: UIView {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
