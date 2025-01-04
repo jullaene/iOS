c0fb32eb362e0e7daa20b21f2716d7a6f7cc2232
hoyeon <fnfn0901@sookmyung.ac.kr>
2025-01-04 20:58:31 +0900

    feat: 플로팅 버튼 클릭 시 다음 화면으로 전환 구현

---
+++ walkmong/walkmong/Presentation/MatchingApply/SupportRequest/ViewController/SupportRequestViewController.swift
@@ -0,0 +1,16 @@
+//
+//  SupportRequestViewController.swift
+//  walkmong
+//
+//  Created by 신호연 on 1/4/25.
+//
+
+import UIKit
+
+class SupportRequestViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+}
