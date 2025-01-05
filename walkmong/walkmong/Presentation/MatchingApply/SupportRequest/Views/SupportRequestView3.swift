4fec3c7425e3aa37104633c241e7acefed334d1f
hoyeon <fnfn0901@sookmyung.ac.kr>
2025-01-05 17:08:31 +0900

    feat: 반려견 정보 추가

--- walkmong/walkmong/Presentation/MatchingApply/SupportRequest/Views/SupportRequestView3.swift
+++ walkmong/walkmong/Presentation/MatchingApply/SupportRequest/Views/SupportRequestView3.swift
@@ -9,5 +9,46 @@ import UIKit
 import SnapKit
 
 final class SupportRequestView3: UIView {
-    
+
+    private let profileContainerView = UIView()
+    private let profileInformationView = DogProfileInformationView()
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupSubviews()
+        setupConstraints()
+        configureView()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    private func setupSubviews() {
+        addSubview(profileContainerView)
+        profileContainerView.addSubview(profileInformationView)
+    }
+
+    private func setupConstraints() {
+        profileContainerView.snp.makeConstraints { make in
+            make.leading.equalToSuperview().offset(-20)
+            make.trailing.equalToSuperview()
+            make.top.equalToSuperview()
+            make.bottom.equalToSuperview()
+        }
+
+        profileInformationView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+    }
+
+    private func configureView() {
+        profileInformationView.configure(
+            isFemale: true,
+            name: "봄별이",
+            informationText: "소형견 · 말티즈 · 4kg",
+            location: "노원구 공릉동",
+            profileImage: UIImage(named: "puppyImage01") ?? UIImage()
+        )
+    }
 }
