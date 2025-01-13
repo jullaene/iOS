53a59ccb9e788bef9c63725bdd2c9d12749baff5
woongaaaa <woonge0930@naver.com>
2025-01-14 01:44:34 +0900

    feat: 뷰컨트롤러 공통 부분 구현

--- walkmong/walkmong/Presentation/RegisterPet/Message/ViewController/RegisterPetMessageViewController.swift
+++ walkmong/walkmong/Presentation/RegisterPet/Message/ViewController/RegisterPetMessageViewController.swift
@@ -6,11 +6,13 @@
 //
 
 import UIKit
+import SnapKit
 
 final class RegisterPetMessageViewController: UIViewController {
     
     private let mainView = RegisterPetMessageView()
     private var keyboardManager: KeyboardEventManager?
+    private var containerBottomConstraint: Constraint?
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,7 +20,7 @@ final class RegisterPetMessageViewController: UIViewController {
         addSubview()
         setConstraints()
         addCustomNavigationBar(titleText: "반려견 등록하기", showLeftBackButton: true, showLeftCloseButton: false, showRightCloseButton: false, showRightRefreshButton: false)
-        addProgressBar(currentStep: 3, totalSteps: 3)
+        addProgressBar(currentStep: 2, totalSteps: 3)
         dismissKeyboardOnTap()
         mainView.delegate = self
         keyboardManager = KeyboardEventManager(delegate: self)
@@ -30,24 +32,33 @@ final class RegisterPetMessageViewController: UIViewController {
     
     private func setConstraints() {
         mainView.snp.makeConstraints { make in
-            make.horizontalEdges.bottom.equalToSuperview()
+            make.horizontalEdges.equalToSuperview()
             make.top.equalToSuperview().offset(156)
+            containerBottomConstraint = make.bottom.equalToSuperview().constraint
         }
     }
+
 }
 
 extension RegisterPetMessageViewController: KeyboardObserverDelegate {
     func keyboardWillShow(keyboardHeight: CGFloat) {
-        
+        containerBottomConstraint?.update(offset: -keyboardHeight)
+        UIView.animate(withDuration: 0.3) {
+            self.view.layoutIfNeeded()
+        }
     }
-    
+
     func keyboardWillHide() {
-        
+        containerBottomConstraint?.update(offset: 0)
+        UIView.animate(withDuration: 0.3) {
+            self.view.layoutIfNeeded()
+        }
     }
+
 }
 
 extension RegisterPetMessageViewController: RegisterPetMessageViewDelegate {
     func didTapNextButton() {
-        
+        // TODO: 데이터 전달 및 화면 전환
     }
 }
--- walkmong/walkmong/Presentation/RegisterPet/Sociality/ViewController/RegisterPetSocialityViewController.swift
+++ walkmong/walkmong/Presentation/RegisterPet/Sociality/ViewController/RegisterPetSocialityViewController.swift
@@ -6,11 +6,13 @@
 //
 
 import UIKit
+import SnapKit
 
 final class RegisterPetSocialityViewController: UIViewController {
 
     private let mainView = RegisterPetMessageView()
     private var keyboardManager: KeyboardEventManager?
+    private var containerBottomConstraint: Constraint?
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,7 +20,7 @@ final class RegisterPetSocialityViewController: UIViewController {
         addSubview()
         setConstraints()
         addCustomNavigationBar(titleText: "반려견 등록하기", showLeftBackButton: true, showLeftCloseButton: false, showRightCloseButton: false, showRightRefreshButton: false)
-        addProgressBar(currentStep: 2, totalSteps: 3)
+        addProgressBar(currentStep: 3, totalSteps: 3)
         dismissKeyboardOnTap()
         mainView.delegate = self
         keyboardManager = KeyboardEventManager(delegate: self)
@@ -30,24 +32,31 @@ final class RegisterPetSocialityViewController: UIViewController {
     
     private func setConstraints() {
         mainView.snp.makeConstraints { make in
-            make.horizontalEdges.bottom.equalToSuperview()
+            make.horizontalEdges.equalToSuperview()
             make.top.equalToSuperview().offset(156)
+            containerBottomConstraint = make.bottom.equalToSuperview().constraint
         }
     }
 }
 
 extension RegisterPetSocialityViewController: KeyboardObserverDelegate {
     func keyboardWillShow(keyboardHeight: CGFloat) {
-        
+        containerBottomConstraint?.update(offset: -keyboardHeight)
+        UIView.animate(withDuration: 0.3) {
+            self.view.layoutIfNeeded()
+        }
     }
-    
+
     func keyboardWillHide() {
-        
+        containerBottomConstraint?.update(offset: 0)
+        UIView.animate(withDuration: 0.3) {
+            self.view.layoutIfNeeded()
+        }
     }
 }
 
 extension RegisterPetSocialityViewController: RegisterPetMessageViewDelegate {
     func didTapNextButton() {
-        
+        // TODO: API 호출
     }
 }
