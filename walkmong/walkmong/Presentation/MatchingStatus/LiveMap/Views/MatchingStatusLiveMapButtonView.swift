cfa91ac6e89c052e45e1284515df053b2bde1575
woongaaaa <woonge0930@naver.com>
2025-01-17 00:08:40 +0900

    design: 진행중인 산책 UI 구현

---
+++ walkmong/walkmong/Presentation/MatchingStatus/LiveMap/Views/MatchingStatusLiveMapButtonView.swift
@@ -0,0 +1,102 @@
+//
+//  MatchingStatusLiveMapButtonView.swift
+//  walkmong
+//
+//  Created by 황채웅 on 1/16/25.
+//
+
+import UIKit
+
+protocol MatchingStatusLiveMapButtonViewDelegate: AnyObject {
+    func didTapContactButton()
+    func didTapEndButton()
+}
+
+final class MatchingStatusLiveMapButtonView: UIView {
+    
+    weak var delegate: MatchingStatusLiveMapButtonViewDelegate?
+    
+    private let isWalker: Bool
+    
+    private let contactButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("산책자 연락하기", for: .normal)
+        button.titleLabel?.font = UIFont(name: "Pretendard-SemiBold", size: 16)
+        button.titleLabel?.textColor = .white
+        button.clipsToBounds = true
+        button.layer.cornerRadius = 15
+        return button
+    }()
+    
+    private lazy var endButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("산책 종료", for: .normal)
+        button.clipsToBounds = true
+        button.titleLabel?.font = UIFont(name: "Pretendard-SemiBold", size: 16)
+        button.titleLabel?.textColor = .white
+        button.layer.cornerRadius = 15
+        button.backgroundColor = .gray600
+        button.setTitleColor(.white, for: .normal)
+        return button
+    }()
+
+    init(isWalker: Bool) {
+        self.isWalker = isWalker
+        super.init(frame: .zero)
+        self.backgroundColor = .white
+        addSubview()
+        setConstraints()
+        setButtonAction()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func addSubview() {
+        if isWalker {
+            addSubview(contactButton)
+        }else {
+            addSubviews(contactButton, endButton)
+        }
+    }
+    
+    func setConstraints() {
+        if isWalker {
+            contactButton.backgroundColor = .gray600
+            contactButton.setTitleColor(.white, for: .normal)
+            contactButton.snp.makeConstraints { make in
+                make.height.equalTo(54)
+                make.horizontalEdges.equalToSuperview().inset(20)
+                make.top.equalToSuperview().offset(12)
+            }
+        }else {
+            contactButton.backgroundColor = .gray300
+            contactButton.setTitleColor(.mainBlack, for: .normal)
+            contactButton.snp.makeConstraints { make in
+                make.height.equalTo(54)
+                make.leading.equalToSuperview().offset(20)
+                make.top.equalToSuperview().offset(12)
+                make.trailing.equalTo(self.snp.centerX).offset(-4)
+            }
+            endButton.snp.makeConstraints { make in
+                make.height.equalTo(54)
+                make.trailing.equalToSuperview().offset(-20)
+                make.top.equalToSuperview().offset(12)
+                make.leading.equalTo(self.snp.centerX).offset(4)
+            }
+        }
+    }
+    private func setButtonAction() {
+        contactButton.addTarget(self, action: #selector(contactButtonAction), for: .touchUpInside)
+        endButton.addTarget(self, action: #selector(endButtonAction), for: .touchUpInside)
+    }
+    
+    @objc private func contactButtonAction() {
+        delegate?.didTapContactButton()
+    }
+    
+    @objc private func endButtonAction() {
+        delegate?.didTapEndButton()
+    }
+}
--- walkmong/walkmong/Presentation/MatchingStatus/LiveMap/Views/MatchingStatusLiveMapView.swift
+++ walkmong/walkmong/Presentation/MatchingStatus/LiveMap/Views/MatchingStatusLiveMapView.swift
@@ -6,15 +6,48 @@
 //
 
 import UIKit
+import NMapsMap
 
 final class MatchingStatusLiveMapView: UIView {
+    
+    let mapView: NMFNaverMapView = {
+        let mapView = NMFNaverMapView()
+        return mapView
+    }()
 
     override init(frame: CGRect) {
         super.init(frame: frame)
+        addSubview()
+        setConstraints()
+        setupMap()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func addSubview() {
+        addSubview(mapView)
+    }
+    
+    private func setConstraints() {
+        mapView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+    }
+    
+    func updateLocation(position: NMGLatLng) {
+        let locationOverlay = mapView.mapView.locationOverlay
+        locationOverlay.location = position
+    }
+    
+    func setupMap() {
+        mapView.showScaleBar = false
+        mapView.showZoomControls = false
+        mapView.mapView.zoomLevel = 15
+        let locationOverlay = mapView.mapView.locationOverlay
+        locationOverlay.icon = NMFOverlayImage(image: .currentLocationIcon)
+        locationOverlay.anchor = CGPoint(x: 0.5, y: 0.5)
     }
 
 }
