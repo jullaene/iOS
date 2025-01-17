82360433aad1956eca7e9b675fe9019dc8692cfb
woongaaaa <woonge0930@naver.com>
2025-01-18 01:45:30 +0900

    feat: 게시글 조회 API 호출부 구현

--- walkmong/walkmong/Presentation/MatchingStatus/LiveMap/ViewController/MatchingStatusLiveMapViewController.swift
+++ walkmong/walkmong/Presentation/MatchingStatus/LiveMap/ViewController/MatchingStatusLiveMapViewController.swift
@@ -14,10 +14,11 @@ final class MatchingStatusLiveMapViewController: UIViewController {
     private lazy var locationManager = CLLocationManager()
     private var Location_Address: CLLocation?
     private var lastMyLocation: CLLocation?
+    private var viewControllerToPresent: MatchingStatusLiveMapModalViewController?
     private let isWalker: Bool
     private let boardId: Int
     private let mapView = MatchingStatusLiveMapView()
-    private let boardService = BoardService()
+    private let applyService = ApplyService()
 
     init(isWalker: Bool, boardId: Int) {
         self.boardId = boardId
@@ -53,8 +54,8 @@ final class MatchingStatusLiveMapViewController: UIViewController {
         }
     }
     private func showSheet(dogNickname: String, walkerNickname: String? = nil) {
-        let viewControllerToPresent = MatchingStatusLiveMapModalViewController(dogNickname: dogNickname, walkerNickname: walkerNickname)
-        viewControllerToPresent.modalPresentationStyle = .pageSheet
+        viewControllerToPresent = MatchingStatusLiveMapModalViewController(dogNickname: dogNickname, walkerNickname: walkerNickname)
+        viewControllerToPresent?.modalPresentationStyle = .pageSheet
         let detentIdentifier = UISheetPresentationController.Detent.Identifier("customDetent")
         let customDetent = UISheetPresentationController.Detent.custom(identifier: detentIdentifier) { _ in
             
@@ -63,7 +64,7 @@ final class MatchingStatusLiveMapViewController: UIViewController {
             
             return 350 - safeAreaBottom
         }
-        if let sheet = viewControllerToPresent.sheetPresentationController {
+        if let sheet = viewControllerToPresent?.sheetPresentationController {
             sheet.detents = [customDetent, .large()] // 사용 가능한 높이 설정
             sheet.selectedDetentIdentifier = customDetent.identifier // 초기 높이 설정
             sheet.prefersGrabberVisible = true // Grabber 표시
@@ -73,7 +74,9 @@ final class MatchingStatusLiveMapViewController: UIViewController {
             sheet.prefersEdgeAttachedInCompactHeight = true // 작은 화면에서도 시트 고정
             sheet.largestUndimmedDetentIdentifier = .large // 배경 흐림 효과 적용
         }
-        present(viewControllerToPresent, animated: true, completion: nil)
+        if let vc = viewControllerToPresent {
+            present(vc, animated: true, completion: nil)
+        }
     }
     
     private func setToGetCurrentLocation() {
@@ -105,7 +108,33 @@ extension MatchingStatusLiveMapViewController: NavigationBarDelegate {
 }
 
 extension MatchingStatusLiveMapViewController {
-    //TODO: 게시글 상세 정보 API 호출
+    
+    private func getBoardDetail() {
+        Task {
+            do {
+//                let response = try await applyService
+//                if isWalker {
+//                    showSheet(dogNickname: response.dogName)
+//                }else {
+//                    showSheet(dogNickname: response.dogName, walkerNickname: response.ownerName)
+//                }
+//                if let vc = viewControllerToPresent {
+//                    vc.setContent(requestMessage: response.walkRequest ?? "", referenceMessage: response.walkNote, additionalMessage: response.additionalRequest ?? "")
+//                    let timeDifference = calculateTimeDifference(startTime: response.startTime, endTime: response.endTime)
+//                    vc.setTime(timePast: timeDifference.timePast, timeLeft: timeDifference.timeLeft)
+//                }
+            }catch let error as NetworkError{
+                CustomAlertViewController
+                    .CustomAlertBuilder(viewController: self)
+                    .setTitleState(.useTitleAndSubTitle)
+                    .setTitleText("매칭 상세 조회 실패")
+                    .setSubTitleText(error.message)
+                    .setButtonState(.singleButton)
+                    .setSingleButtonTitle("돌아가기")
+                    .showAlertView()
+            }
+        }
+    }
     
     private func fetchCurrentLocation() {
         guard let lat = Location_Address?.coordinate.latitude,
@@ -159,5 +188,44 @@ extension MatchingStatusLiveMapViewController: CLLocationManagerDelegate {
 extension MatchingStatusLiveMapViewController: UISheetPresentationControllerDelegate {
     func presentationControllerShouldDismiss(_ presentationController: UIPresentationController) -> Bool {
         return false
+    }
+}
+
+extension MatchingStatusLiveMapViewController {
+    private func calculateTimeDifference(startTime: String, endTime: String) -> (timePast: Int, timeLeft: Int) {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "HH:mm"
+        
+        // 현재 시간
+        let now = Date()
+        let calendar = Calendar.current
+        
+        // 문자열로부터 Date 생성
+        guard let startDate = dateFormatter.date(from: startTime),
+              let endDate = dateFormatter.date(from: endTime) else {
+            return (0,0)
+        }
+        
+        // 현재 시간의 "HH:mm"으로만 비교할 수 있도록 오늘 날짜와 합침
+        let nowComponents = calendar.dateComponents([.hour, .minute], from: now)
+        let todayStartTime = calendar.date(bySettingHour: calendar.component(.hour, from: startDate),
+                                           minute: calendar.component(.minute, from: startDate),
+                                           second: 0,
+                                           of: now) ?? startDate
+        let todayEndTime = calendar.date(bySettingHour: calendar.component(.hour, from: endDate),
+                                         minute: calendar.component(.minute, from: endDate),
+                                         second: 0,
+                                         of: now) ?? endDate
+        
+        // 경과 시간 (분 단위)
+        let minutesPast = calendar.dateComponents([.minute], from: todayStartTime, to: now).minute ?? 0
+        // 남은 시간 (분 단위)
+        let minutesLeft = calendar.dateComponents([.minute], from: now, to: todayEndTime).minute ?? 0
+        
+        // 결과 생성
+        let timePastText = minutesPast > 0 ? minutesPast: 0
+        let timeLeftText = minutesLeft > 0 ? minutesLeft : 0
+        
+        return (timePastText, timeLeftText)
     }
 }
